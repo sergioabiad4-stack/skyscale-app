31179dc026ae2c4468d1ef3cbdab2ea078441593
sergioabiad4-stack <sergioabiad4@gmail.com>
2026-08-25 11:29:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import chatHandler from './api/chat.js';
 import driveHandler from './api/drive.js';
+import deckHandler from './api/deck.js';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static(__dirname));
 
 app.post('/api/chat', chatHandler);
 app.post('/api/drive', driveHandler);
+app.post('/api/deck', deckHandler);
 
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'index.html')));
 
